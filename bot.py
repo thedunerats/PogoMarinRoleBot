7bb969f59bbb53a38a70b89500665f4e76799841
thedunerats <thedunerats@yahoo.com>
2023-04-06 13:56:28 -0700

    More permitted roles

--- bot.py
+++ bot.py
@@ -35,7 +35,8 @@ async def on_message(message):
                        'lvl50', 'lvl49', 'lvl48', 'lvl47', 'lvl46', 'lvl45', 'lvl44', 'lvl43', 'lvl42', 'lvl41',
                        'lvl40', 'lvl39', 'lvl38', 'lvl37', 'lvl36', 'lvl35', 'lvl34', 'lvl33', 'lvl32', 'lvl31',
                        'lvl30', 'lvl29', 'lvl28', 'lvl27', 'lvl26', 'lvl25', 'lvl24', 'lvl23', 'lvl22',
-                       'ttar', 'ditto', 'machamp', 'kecleon']
+                       'ttar', 'ditto', 'machamp', 'kecleon', 'chansey', 'axew', 'deino', 'unown', 'lapras',
+                       'ditto']
         if message.content.startswith('!r '):
             split_message = message.content[3:].split(', ')
             requested_roles = []
